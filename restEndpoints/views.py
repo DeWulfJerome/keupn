2ed45b886e2e68bd24ca6322a38d8480c3e48434
thomasgijs <36015515+thomasgijs@users.noreply.github.com>
2020-04-07 14:18:43 +0200

    added IsAuthenticated restriction

--- restEndpoints/views.py
+++ restEndpoints/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
-
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def index(request):
     return HttpResponse("Yeet koniño")
